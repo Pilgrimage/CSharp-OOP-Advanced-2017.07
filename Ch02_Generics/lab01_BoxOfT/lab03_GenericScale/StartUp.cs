08675857293bcaee1bbf9a76bbeb7fa21dbaaa7c
Pilgrimage <nanobot149@gmail.com>
2017-07-25 18:45:52 +0300

    Solved problems 11, 12 and small another changes.

---
+++ Ch02_Generics/lab01_BoxOfT/lab03_GenericScale/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Scale<String> stringScale = new Scale<String>("a", "c");
+        var result1 = stringScale.GetHavier();
+        if (result1!=null)
+        {
+            Console.WriteLine(result1);
+        }
+
+        Scale<Int32> intScale = new Scale<Int32>(1, 2);
+        var result2 = intScale.GetHavier();
+        if (result2 != null)
+        {
+            Console.WriteLine(result2);
+        }
+
+    }
+}
---
+++ Ch02_Generics/p11_Tuple/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace p11_Tuple
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] inParams = Console.ReadLine().Split(' ');
+            string names = $"{inParams[0]} {inParams[1]}";
+            string address = inParams[2];
+            Tuple<string, string> namesAdress = new Tuple<string, string>(names, address);
+            Console.WriteLine(namesAdress);
+
+            inParams = Console.ReadLine().Split(' ');
+            string name = inParams[0];
+            int liters = int.Parse(inParams[1]);
+            Tuple<string, int> nameLiters = new Tuple<string, int>(name, liters);
+            Console.WriteLine(nameLiters);
+
+            inParams = Console.ReadLine().Split(' ');
+            int intParam = int.Parse(inParams[0]);
+            double doubleParam = double.Parse(inParams[1]);
+            Tuple<int, double> intDouble = new Tuple<int, double>(intParam, doubleParam);
+            Console.WriteLine(intDouble);
+        }
+    }
+}
