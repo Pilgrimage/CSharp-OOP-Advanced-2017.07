22f0fdff459d2c635c0c5e25a8498e8ed81420aa
Pilgrimage <nanobot149@gmail.com>
2017-07-30 15:26:35 +0300

    Enums and Attributes - initial & lab

---
+++ Ch04_EnumsAndAttributes/lab01_WeekDay/WeeklyEntry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class WeeklyEntry : IComparable<WeeklyEntry>
+{
+    private WeekDay weekDay;
+
+    public WeeklyEntry(string weekday, string notes)
+    {
+        Enum.TryParse(weekday, out this.weekDay);
+        this.Notes = notes;
+    }
+
+    public WeekDay Weekday => this.weekDay;
+    public string Notes { get; private set; }
+
+    public int CompareTo(WeeklyEntry other)
+    {
+        if (ReferenceEquals(this,other))
+        {
+            return 0;
+        }
+
+        if (ReferenceEquals(null, other))
+        {
+            return 1;
+        }
+
+        int weekdayComparision = this.weekDay.CompareTo(other.weekDay);
+        if (weekdayComparision!=0)
+        {
+            return weekdayComparision;
+        }
+
+        return string.Compare(this.Notes, other.Notes, StringComparison.Ordinal);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Weekday} - {this.Notes}";
+    }
+
+}
---
+++ Ch04_EnumsAndAttributes/lab02_CoffeeMachine/CoffeeMachine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CoffeeMachine
+{
+    private int coins;
+    private IList<CoffeeType> coffeesSold;
+
+    public CoffeeMachine()
+    {
+        this.coffeesSold = new List<CoffeeType>();
+    }
+
+    public IEnumerable<CoffeeType> CoffeesSold
+    {
+        get { return this.coffeesSold; }
+    }
+    
+    public void BuyCoffee(string size, string type)
+    {
+        CoffeeType coffeeType = (CoffeeType)Enum.Parse(typeof(CoffeeType), type);
+        CoffeePrice coffeePrice = (CoffeePrice)Enum.Parse(typeof(CoffeePrice), size);
+        if (this.coins >= (int)coffeePrice)
+        {
+            this.coffeesSold.Add(coffeeType);
+            this.coins = 0;
+        }
+    }
+
+    public void InsertCoin(string coin)
+    {
+        Coin monetCoin = (Coin)Enum.Parse(typeof(Coin), coin);
+        this.coins += (int)monetCoin;
+    }
+
+}
