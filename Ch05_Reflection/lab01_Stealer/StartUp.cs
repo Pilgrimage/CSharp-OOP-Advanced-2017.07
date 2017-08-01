b32ea610a67e8269284a4aee4f6e4acecddc60a3
Pilgrimage <nanobot149@gmail.com>
2017-08-01 23:42:30 +0300

    Lab is done

---
+++ Ch05_Reflection/lab01_Stealer/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+
+        // lab01_Stealer
+        //string result = spy.StealFieldInfo("Hacker", "username", "password");
+
+        // lab02_HighQualityMistakes
+        //string result = spy.AnalyzeAcessModifiers("Hacker");
+
+        // lab03_MissionPrivateImpossible
+        //string result = spy.RevealPrivateMethods("Hacker");
+
+        // lab04_Collector
+        string result = spy.CollectGettersAndSetters("Hacker");
+
+
+        Console.WriteLine(result);
+    }
+}
+
